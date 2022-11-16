f974365435e6412765149a5e9d4074833c861df1
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-11-17 00:04:09 +0300

    Update task17.py

--- python/dz4/task_17/task17.py
+++ python/dz4/task_17/task17.py
@@ -1,13 +1,14 @@
-RUBLE=1
-DOLLAR=60
-EURO=70
+RUBLE = 1
+DOLLAR = 60
+EURO = 70
 
 
 class BaseWallet:
-    exchange_rate=1
+    exchange_rate = 1
+
     def __init__(self, name, amount):
-        self.name=name
-        self.amount=amount
+        self.name = name
+        self.amount = amount
 
     def get_name(self):
         return self.name
@@ -18,16 +19,17 @@ class BaseWallet:
     def get_exchange_cofficient(self):
         return self.exchange_rate
 
-    def get_base_amount(self):
+    def to_base(self):
         return self.exchange_rate * self.amount
 
     def repeating_operation(self, other, operation):
         if isinstance(other, BaseWallet):
-            return self.__class__(self.name, operation(self.get_base_amount(), other.get_base_amount()) // self.exchange_rate)
+            return self.__class__(self.name,
+                                  operation(self.to_base(), other.to_base()) / self.exchange_rate)
         return self.__class__(self.name, operation(self.amount, other))
 
     def __add__(self, other):
-        return self.repeating_operation(other, lambda a, b: a+b)
+        return self.repeating_operation(other, lambda a, b: a + b)
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -36,16 +38,16 @@ class BaseWallet:
         return self.__add__(other)
 
     def __sub__(self, other):
-        return self.repeating_operation(other, lambda a, b: a-b)
+        return self.repeating_operation(other, lambda a, b: a - b)
 
     def __rsub__(self, other):
-        return self.__sub__(other)
+        return self.repeating_operation(other, lambda a, b: b - a)
 
     def __isub__(self, other):
-        return  self.__sub__(other)
+        return self.__sub__(other)
 
     def __mul__(self, other):
-        return self.repeating_operation(other, lambda a, b: a*b)
+        return self.repeating_operation(other, lambda a, b: a * b)
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -54,34 +56,38 @@ class BaseWallet:
         return self.__mul__(other)
 
     def __truediv__(self, other):
-        return self.repeating_operation(other, lambda a, b: a/b)
+        return self.repeating_operation(other, lambda a, b: a / b)
 
     def __rtruediv__(self, other):
-        return self.repeating_operation(other, lambda a, b: b/a)
+        return self.repeating_operation(other, lambda a, b: b / a)
 
     def __eq__(self, other):
         return type(self) is type(other) and self.amount == other.amount
 
-    def __str__(self):
-        return f"{self.__class__} {self.name} {self.amount}"
+    def __repr__(self):
+        return f"{self._log_name} {self.name} {self.amount}"
 
     def spend_all(self):
         if self.get_amount() > 0:
-            self._amount = 0
+            self.amount = 0
+
 
 class RubbleWallet(BaseWallet):
     def __init__(self, name, amount):
         super().__init__(name, amount)
-        self.exchange_rate=RUBLE
+        self.exchange_rate = RUBLE
+        self._log_name = "Rubble Wallet"
 
 
 class DollarWallet(BaseWallet):
     def __init__(self, name, amount):
         super().__init__(name, amount)
-        self.exchange_rate=DOLLAR
+        self.exchange_rate = DOLLAR
+        self._log_name = "Dollar Wallet"
 
 
 class EuroWallet(BaseWallet):
     def __init__(self, name, amount):
         super().__init__(name, amount)
-        self.exchange_rate=EURO
+        self.exchange_rate = EURO
+        self._log_name = "Euro Wallet"
